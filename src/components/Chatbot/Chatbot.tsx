387dec4fe78077675afe036b3fa3fc04626beb24
Maksym Vizniak <razor5826@gmail.com>
2023-05-04 11:48:33 +0300

    summary hotfix

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -36,8 +36,8 @@ export const Chatbot = () => {
   const prompt = PromptTemplate.fromTemplate(`
     The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.     Current conversation:
      {chat_history}
-     Human: {value}
-     AI:`);
+     {value}
+     `);
 
   const userStoryPrompt = PromptTemplate.fromTemplate(`
       Give a summary of this chat history in form only from user view "As a user I want to: "
