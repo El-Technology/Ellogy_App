465bd8daf268ef1b4cb96e9f62dec9f033a06cdf
mxtokar <mxtokar@gmail.com>
2023-06-20 09:40:48 +0300

    added auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React ,{useEffect} from "react";
-import {useTranslation} from "react-i18next";
-import {Route, Routes} from "react-router-dom";
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import {ROUTES} from "./core/constants/routes";
-import {Home} from "./pages/Home";
-import {Login} from "./pages/Login/Login";
-import {SignUp} from "./pages/SignUp/SignUp";
-import {ProtectedRoutes} from "./hoc/ProtectedRoutes";
-import {ResetPassword} from "./pages/ResetPassword/ResetPassword";
+import { ROUTES } from "./core/constants/routes";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login/Login";
+import { SignUp } from "./pages/SignUp/SignUp";
+import { ProtectedRoutes } from "./hoc/ProtectedRoutes";
+import { ResetPassword } from "./pages/ResetPassword/ResetPassword";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
   useEffect(() => {
     if (i18n.language === "ar") {
       document.body.classList.add("rtl");
@@ -22,12 +22,12 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path={ROUTES.LOGIN} element={<Login/>}/>
-      <Route path={ROUTES.SIGNUP} element={<SignUp/>}/>
-      <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword/>}/>
-      <Route path={ROUTES.CREATE_NEW_PASSWORD} element={<ResetPassword/>}/>
-      <Route element={<ProtectedRoutes/>}>
-        <Route path={ROUTES.HOME} element={<Home/>}/>
+      <Route path={ROUTES.LOGIN} element={<Login />} />
+      <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+      <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
+      <Route path={ROUTES.CREATE_NEW_PASSWORD} element={<ResetPassword />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path={ROUTES.HOME} element={<Home />} />
       </Route>
     </Routes>
   );
