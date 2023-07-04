1e99e49f579f7848f92547495fbe6f91b258c592
mxtokar <mxtokar@gmail.com>
2023-07-04 09:20:04 +0300

    changed draft flow

--- src/components/Sidebars/Sidebar.tsx
+++ src/components/Sidebars/Sidebar.tsx
@@ -5,86 +5,98 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { ReactComponent as Add } from "../../assets/icons/add.svg";
-import { ReactComponent as MessageQuestion } from "../../assets/icons/message-question.svg";
-import { ReactComponent as Search } from "../../assets/icons/search.svg";
-import { useTranslation } from "react-i18next";
+import {ReactComponent as Add} from "../../assets/icons/add.svg";
+import {ReactComponent as MessageQuestion} from "../../assets/icons/message-question.svg";
+import {ReactComponent as Search} from "../../assets/icons/search.svg";
+import {useTranslation} from "react-i18next";
 import React, {useEffect, useRef, useState} from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import cx from 'classnames';
+import {getTicketsByUserId, searchTickets} from "../../store/ticket-service/asyncActions";
 import {
-  createTicket,
-  getTicketsByUserId,
-} from "../../store/ticket-service/asyncActions";
-import {
-  getActiveTicket,
+  getActiveTicket, getIsTicketUpdate,
   getTickets,
   getTicketsLoader,
 } from "../../store/ticket-service/selector";
-import { Oval } from "react-loader-spinner";
-import { format } from "date-fns";
-import {setActiveTicket} from "src/store/ticket-service/ticketSlice";
-import { Statuses } from "src/core/enums/common";
-import styles  from './Sidebar.module.scss';
-import { DraftModal } from "./DrafModal";
+import {Oval} from "react-loader-spinner";
+import {format} from "date-fns";
+import {addLocalTicket, setActiveTicket} from "src/store/ticket-service/ticketSlice";
+import {Statuses} from "src/core/enums/common";
+import styles from './Sidebar.module.scss';
+import {DraftModal} from "./DrafModal";
+import {useAppDispatch} from "../../store/store";
+import useDebounce from "../../core/hooks/useDebounce";
 
 export const Sidebar = () => {
-  const { t } = useTranslation("navigation");
-  const dispatch: any = useDispatch();
+  const {t} = useTranslation("navigation");
+  const dispatch: any = useAppDispatch();
   const tickets = useSelector(getTickets);
   const loader = useSelector(getTicketsLoader);
   const activeTicket = useSelector(getActiveTicket);
-  const [anotherTicket, setAnotherTicket]= useState(null);
+  const isTicketUpdate = useSelector(getIsTicketUpdate)
   const [isShowDraft, setIsShowDraft] = useState(false);
-  
+  const [isCreating, setIsCreating] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
   const handleCloseModal = () => {
     setIsShowDraft(false)
   }
+
   const storedUser = localStorage.getItem("user");
   const user = storedUser ? JSON.parse(storedUser) : null;
 
   useEffect(() => {
-    // @ts-ignore
+    const recordsPerPage = tickets.length;
+    if (debouncedSearchQuery.length)
+      dispatch(searchTickets({userId: user.id, ticketTitle: debouncedSearchQuery}));
+  }, [debouncedSearchQuery]);
+
+  useEffect(() => {
     dispatch(getTicketsByUserId({userId: user.id}));
   }, []);
 
   useEffect(() => {
     if (tickets.length > 0 && !activeTicket) {
-      // @ts-ignore
       dispatch(setActiveTicket(tickets[0]));
     }
   }, [tickets, activeTicket]);
 
-  const createNewRequest = () => {
-    if(activeTicket) {
-      setIsShowDraft(true);
-      return;
-    }
+  const createLocalTicket = () => {
+    if (activeTicket) {
+      if (!activeTicket.id) {
+        setIsShowDraft(true);
+        setIsCreating(true);
+        return;
+      }
 
-    console.log('activeticket', activeTicket)
-    // const defaultTicket = {
-    //   title: "New request",
-    //     description:
-    //       "We will generate a description automatically as soon as we get some information from you.\n\nYou can change the title and description at any time.",
-    //     createdDate: new Date().toISOString(),
-    //     comment: null,
-    //     ticketMessages: [],
-    // }
-    // @ts-ignore
-    dispatch(createTicket(user.id))
-      .then(() => dispatch(getTicketsByUserId({userId: user.id})))
-      .then((res: any) => {
-        dispatch(setActiveTicket(res.payload?.data[0]));
-      });
-  };
+      if (isTicketUpdate) {
+        console.log("2")
+        setIsShowDraft(true);
+        setIsCreating(true);
+        return;
+      }
+
+      const defaultTicket = {
+        title: "New request",
+        description:
+          "We will generate a description automatically as soon as we get some information from you. You can change the title and description at any time.",
+        createdDate: new Date().toISOString(),
+        comment: null,
+        messages: [],
+        status: 0
+      }
+      dispatch(addLocalTicket(defaultTicket))
+      dispatch(setActiveTicket(defaultTicket));
+    }
+  }
 
   const handleTicketClick = (ticket: any) => {
-    if(activeTicket && ticket.id !== activeTicket.id) {
-      setAnotherTicket(ticket)
+    if (isTicketUpdate) {
       setIsShowDraft(true);
-      return;
-    }
-    dispatch(setActiveTicket(ticket));
+      setIsCreating(false);
+    } else
+      dispatch(setActiveTicket(ticket));
   };
 
   const containerRef = useRef<HTMLDivElement>(null);
@@ -92,18 +104,22 @@ export const Sidebar = () => {
 
   useEffect(() => {
     const container = containerRef.current;
+    if (!debouncedSearchQuery.length) {
+      dispatch(getTicketsByUserId({userId: user.id}));
+    }
 
     const handleScroll = async () => {
-      if (container) {
+      if (container && !debouncedSearchQuery.length) {
         const isAtBottom = container.scrollHeight - container.scrollTop === container.clientHeight;
         if (isAtBottom) {
           const nextPage = currentPageRef.current + 1;
-          dispatch(getTicketsByUserId({ userId: user.id, currentPageNumber: nextPage }))
+          console.log(nextPage)
+          dispatch(getTicketsByUserId({userId: user.id, currentPageNumber: nextPage}))
             .then((action: any) => {
               const newTickets = action.payload.data;
               if (newTickets.length > 0) {
                 currentPageRef.current = nextPage;
-              }
+              } else currentPageRef.current = 1;
             })
             .catch((error: any) => {
               console.log(error)
@@ -116,184 +132,189 @@ export const Sidebar = () => {
     return () => {
       container?.removeEventListener("scroll", handleScroll);
     };
-  }, [user.id]);
+  }, [user.id, debouncedSearchQuery]);
 
   return (
     <>
-    
-    <Box
-      className="rtl-able"
-      sx={{
-        maxWidth: "650px",
-        width: "100%",
-        height: "calc(100vh - 82px)",
-        padding: "40px 24px 45px",
-        display: {
-          xs: "none",
-          sm: "flex",
-          md: "flex",
-          xl: "flex",
-        },
-        flexDirection: "column",
-        alignItems: "end",
-        justifyContent: "space-between",
-        backgroundColor: "#FBFBFB",
-        boxShadow: "0px 8px 24px 0px rgba(40, 103, 131, 0.08)",
-        boxSizing: "border-box",
-      }}
-    >
       <Box
+        className="rtl-able"
         sx={{
-          display: "flex",
+          width: "35%",
+          height: "calc(100vh - 82px)",
+          padding: "40px 24px 45px",
+          display: {
+            xs: "none",
+            sm: "flex",
+            md: "flex",
+            xl: "flex",
+          },
           flexDirection: "column",
-          gap: "24px",
-          maxHeight: "80%"
+          alignItems: "end",
+          justifyContent: "space-between",
+          backgroundColor: "#FBFBFB",
+          boxShadow: "0px 8px 24px 0px rgba(40, 103, 131, 0.08)",
+          boxSizing: "border-box",
         }}
       >
-        {loader ? (
-          <Button
-            sx={{
-              height: "44px",
-              width: "251px",
-              borderRadius: "8px",
-              textTransform: "none",
-              fontSize: "16px",
-              fontWeight: "700",
-              gap: "8px",
-            }}
-            variant="contained"
-            color="primary"
-          >
-            <Oval
-              height={24}
-              width={24}
-              color="#fff"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-              ariaLabel="oval-loading"
-              secondaryColor="#91B6FF"
-              strokeWidth={5}
-              strokeWidthSecondary={5}
-            />
-          </Button>
-        ) : (
-          <Button
-            sx={{
-              height: "44px",
-              width: "251px",
-              borderRadius: "8px",
-              textTransform: "none",
-              fontSize: "16px",
-              fontWeight: "700",
-              gap: "8px",
-            }}
-            variant="contained"
-            color="primary"
-            onClick={createNewRequest}
-          >
-            <Add />
-            Create new request
-          </Button>
-        )}
-
-        <TextField
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            ),
-            style: {
-              height: "44px",
-              borderRadius: "8px",
-              background: "#F3F4F5",
-            },
-          }}
+        <Box
           sx={{
-            border: "none",
+            display: "flex",
+            flexDirection: "column",
+            gap: "24px",
+            maxHeight: "80%"
+          }}
+        >
+          {loader ? (
+            <Button
+              sx={{
+                height: "44px",
+                width: "251px",
+                borderRadius: "8px",
+                textTransform: "none",
+                fontSize: "16px",
+                fontWeight: "700",
+                gap: "8px",
+              }}
+              variant="contained"
+              color="primary"
+            >
+              <Oval
+                height={24}
+                width={24}
+                color="#fff"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#91B6FF"
+                strokeWidth={5}
+                strokeWidthSecondary={5}
+              />
+            </Button>
+          ) : (
+            <Button
+              sx={{
+                height: "44px",
+                width: "251px",
+                borderRadius: "8px",
+                textTransform: "none",
+                fontSize: "16px",
+                fontWeight: "700",
+                gap: "8px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={createLocalTicket}
+            >
+              <Add/>
+              Create new request
+            </Button>
+          )}
+
+          <TextField
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search/>
+                </InputAdornment>
+              ),
+              style: {
+                height: "44px",
+                borderRadius: "8px",
+                background: "#F3F4F5",
+              },
+            }}
+            sx={{
+              border: "none",
+              height: "44px",
+              width: "251px",
+              textTransform: "none",
+              fontSize: "16px",
+              fontWeight: "700",
+              gap: "8px",
+
+              "& fieldset": {border: "none"},
+              "& input::placeholder": {color: "#8790A0", opacity: 1},
+            }}
+            placeholder="Search"
+            variant="outlined"
+            color="primary"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+          <Box
+            ref={containerRef}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "16px",
+              overflowY: "auto"
+            }}
+          >
+            {tickets &&
+              tickets.map((item: any, index) => {
+                return (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      cursor: "pointer",
+                      width: "227px",
+                      height: "49px",
+                      background: item === activeTicket ? "#ECF3FF" : "#fff",
+                      borderRadius: "8px",
+                      padding: "8px 12px",
+                      justifyContent: 'space-between',
+                      alignItems: 'center'
+                    }}
+                    key={JSON.stringify(item) + index}
+                    onClick={() => handleTicketClick(item)}
+                  >
+                    <Box sx={{
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+
+                    }}>
+                      <Typography sx={{fontWeight: "700"}}>
+                        {item.title}
+                      </Typography>
+                      <Typography sx={{color: "#707A8E"}}>
+                        {format(new Date(item.createdDate), "dd/MM/yyyy")}
+                      </Typography>
+                    </Box>
+                    {item && <Box>
+                        <Typography className={cx(styles.status, styles[`status_${Statuses[item.status].toLowerCase()}`])}>{Statuses[item.status]}</Typography>
+                    </Box>}
+                  </Box>
+                );
+              })}
+          </Box>
+        </Box>
+
+        <Button
+          sx={{
+            marginTop: "24px",
             height: "44px",
             width: "251px",
+            borderRadius: "8px",
             textTransform: "none",
             fontSize: "16px",
             fontWeight: "700",
             gap: "8px",
-
-            "& fieldset": { border: "none" },
-            "& input::placeholder": { color: "#8790A0", opacity: 1 },
           }}
-          placeholder="Search"
           variant="outlined"
           color="primary"
-        />
-
-        <Box
-          ref={containerRef}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "16px",
-            overflowY: "auto"
-          }}
         >
-          {tickets &&
-            tickets.map((item: any) => {
-              return (
-                <Box
-                  sx={{
-                    display: "flex",
-                    cursor: "pointer",
-                    width: "227px",
-                    height: "49px",
-                    background: item === activeTicket ? "#ECF3FF" : "#fff",
-                    borderRadius: "8px",
-                    padding: "8px 12px",
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                  }}
-                  key={item.id}
-                  onClick={() => handleTicketClick(item)}
-                >
-                  <Box sx={{
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                    
-                  }}>
-                    <Typography sx={{ fontWeight: "700" }}>
-                      {item.title}
-                    </Typography>
-                    <Typography sx={{ color: "#707A8E" }}>
-                      {format(new Date(item.createdDate), "dd/MM/yyyy")}
-                    </Typography>
-                  </Box>
-                  {item && <Box>
-                    <Typography className={cx(styles.status, styles[`status_${Statuses[item.status].toLowerCase()}`])}>{Statuses[item.status]}</Typography>
-                  </Box>}
-                </Box>
-              );
-            })}
-        </Box>
+          <MessageQuestion/>
+          Help & Support
+        </Button>
       </Box>
-
-      <Button
-        sx={{
-          marginTop: "24px",
-          height: "44px",
-          width: "251px",
-          borderRadius: "8px",
-          textTransform: "none",
-          fontSize: "16px",
-          fontWeight: "700",
-          gap: "8px",
-        }}
-        variant="outlined"
-        color="primary"
-      >
-        <MessageQuestion />
-        Help & Support
-      </Button>
-    </Box>
-      {isShowDraft && activeTicket && <DraftModal handleCloseModal={handleCloseModal} ticket={activeTicket} userId={user.id} anotherTicket={anotherTicket}/>}
+      {isShowDraft && activeTicket && <DraftModal
+          handleCloseModal={handleCloseModal}
+          ticket={activeTicket}
+          userId={user.id}
+          isCreating={isCreating}
+      />}
     </>
   );
 };
--- src/layouts/MainPageLayout.tsx
+++ src/layouts/MainPageLayout.tsx
@@ -10,57 +10,33 @@ export const MainPageLayout = ({children}: { children: ReactNode }) => {
 
       <Box
         sx={{
-          display: "flex",
-          width: {
-            xs: "100%",
-            sm: "calc(100vw - 650px)",
-            md: "calc(100vw - 650px)",
-            xl: "calc(100vw - 650px)",
-          },
-          overflowY: "auto",
-          overflowX: "hidden",
-          height: "100%",
-          maxHeight: {
-            xs: "calc(100vh - 82px)",
-            sm: "calc(100vh - 82px)"
-          },
-          position: "absolute",
-          left: {
-            xs: 0,
-            sm: "650px",
-            md: "650px",
-            xl: "650px",
-          },
-          paddingLeft: {
-            xs: "24px",
-            sm: "24px",
-            md: "24px",
-            xl: "24px",
-          },
-          paddingTop: "20px",
-          paddingRight: {
-            xs: "24px",
-            sm: "82px",
-            md: "82px",
-            xl: "82px",
-          },
-          paddingBottom: "24px",
-          backgroundColor: "#EFEFEF",
-          boxSizing: "border-box",
+          display: "flex"
         }}
       >
+        <Sidebar/>
+
         <Box
-          position="relative"
-          display={"flex"}
-          flexDirection={"column"}
-          flexGrow={1}
-          height="100%"
+          sx={{
+            display: "flex",
+            overflowY: "auto",
+            overflowX: "hidden",
+            height: "calc(100vh - 82px)",
+            padding: "20px 24px",
+            backgroundColor: "#EFEFEF",
+            boxSizing: "border-box",
+          }}
         >
-          {children}
+          <Box
+            position="relative"
+            display={"flex"}
+            flexDirection={"column"}
+            flexGrow={1}
+            height="100%"
+          >
+            {children}
+          </Box>
         </Box>
       </Box>
-
-      <Sidebar/>
     </Box>
   );
 };
